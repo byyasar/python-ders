e4d0dfd32c2ac604b90dd5ab8cedeb0d31e62c78
Yaşar BAYKUŞ <45915186+byyasar@users.noreply.github.com>
2021-05-06 03:23:32 +0300

    datetime kullanımı

---
+++ datetime1.py
@@ -0,0 +1,42 @@
+from datetime import datetime,date,timedelta
+# an=datetime.now()
+bugun=date.today()
+dun=bugun+timedelta(days=30)
+print(dun.strftime('%c'))
+
+
+
+
+
+# kacinci_gun=datetime.today().weekday()
+
+# ay=datetime.today().month
+# gun=datetime.today().day
+# saat=datetime.today().hour
+# dakika=datetime.today().minute
+# saniye=datetime.today().second
+
+
+
+
+
+# kullanıcı dogum yılını girdiginde bugun kaç yaşında olduğunu yazdıran kodları yazınız.
+# dogum_yili=int(input('Dogum yılınızı giriniz :'))
+# yil=datetime.today().year
+# print(f'Yaşınız : {yil-dogum_yili}')
+
+
+
+# print(f'tarih saat :{an}')
+# print(f'gunumuz :{bugun}')
+# print(f'kaçıncı gün :{kacinci_gun}')
+
+# gunler=['ptesi','salı','çrş']
+# print(gunler[kacinci_gun])
+# print(f'yıl :{yil}')
+# print(f'ay :{ay}')
+
+
+
+
+
---
+++ datetime2.py
@@ -0,0 +1,26 @@
+import datetime
+import locale
+# locale.setlocale(locale.LC_ALL,'Turkish_Turkey.1254')
+locale.setlocale(locale.LC_ALL,"tr_TR")
+# an = datetime.datetime.now()
+tarih_metni = '4 Mayıs 2021 saat 14:32:11'
+print("Metin halindeki tarih = ", tarih_metni)
+
+tarih_nesnesi = datetime.datetime.strptime(tarih_metni,'%d %B %Y saat %H:%M:%S')
+print("Tarih nesnesi = ",tarih_nesnesi) 
+# print(tarih_nesnesi.year)
+# print(tarih_nesnesi.month)
+print(tarih_nesnesi.strftime('%A'))
+
+
+
+
+
+# print(an.strftime('%Y')) # Yıl
+# print(an.strftime('%X')) # Saat
+# print(an.strftime('%d')) # Gün - ayın kaçıncı günü
+# print(an.strftime('%A')) # Gün - İsim olarak
+# print(an.strftime('%B')) # Ay - İsim olarak
+# print(an.strftime("%d-%m-%Y"))
+# print(an.strftime("%d %B %Y %A"))
+# print(an.strftime("%X"))
